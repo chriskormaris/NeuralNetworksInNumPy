990e48f89ae583d05477e77ee084bfa3fe0c866f
Christos Kormaris <chriskormaris@gmail.com>
2024-05-14 22:04:18 +0300

    Code maintenance

--- neural_networks_for_spam_ham_classification/main.py
+++ neural_networks_for_spam_ham_classification/main.py
@@ -8,8 +8,12 @@ if __name__ == '__main__':
 
     # get train and test data
     print("Reading train and test files...")
-    X_train, y_train, X_test, y_test = get_classification_data(path, feature_dictionary_dir,
-                                                               construct_data=True, tfidf=False)
+    X_train, y_train, X_test, y_test = get_classification_data(
+        path,
+        feature_dictionary_dir,
+        construct_data=True,
+        tfidf=False
+    )
 
     print("X_train length: ")
     print(X_train.shape)
